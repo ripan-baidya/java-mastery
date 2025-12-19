6ee7792f2dae220963d9c49b6284c9b59c56cb6d
Ripan Baidya <ripanbaidya7980@gmail.com>
2025-12-20 00:42:24 +0530

    hello world

--- src/basics/Main.java
+++ src/basics/Main.java
@@ -1,9 +1,7 @@
 package basics;
 
-/**
- * The {@code Main} class is the entry point of the Java program and demonstrates the "Hello World!" program.
- * <p>It utilizes the {@code System.out.println()} method to print the message "Hello World!" to the console.
- * This is the starting point of any Java program.
+/*
+ * Main class that demonstrates the "Hello World!" program.
  */
 public class Main {
     public static void main(String[] args) {

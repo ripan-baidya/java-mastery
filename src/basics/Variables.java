f425f2d0ed6d8bb7ae44f177048f690c20060a81
Ripan Baidya <ripanbaidya7980@gmail.com>
2025-12-20 00:14:22 +0530

    variables

---
+++ src/basics/Variables.java
@@ -0,0 +1,34 @@
+package basics;
+
+/*
+ * In Java, a variable is a named storage location that holds a value.
+ * The variables in Java can be classified into two types:
+ * - Primitive Variables: These are the built-in data types like int, float, char, boolean, etc.
+ * - Reference Variables: These are used to store the memory address of an object.
+ *
+ * Rule to write variables in Java:
+ * 1. Variable names should start with a letter or an underscore or dollar sign.
+ * 2. Variable names should consist of letters, digits, and underscores.
+ * 3. Variable names are case-sensitive.
+ * 4. Variable names should not start with a numeric digit.
+ * 5. Keywords and reserved words in Java should not be used as variable names.
+ * 6. Variable names should be meaningful and descriptive.
+ * 7. It is a good practice to use lowerCamelCase for variable names.
+ * Variables:
+ */
+public class Variables {
+    public static void main(String[] args) {
+
+        // Valid variable names
+        int _age = 25;
+        int $salary = 5000;
+        int employeeId = 12345;
+
+        // Invalid variable names
+        // int 2ndYear = 2022;
+        // int class = 12;
+        // int break = 12;
+
+        // Exercise: Try to write some valid and invalid variable names
+    }
+}

5124dfe4afb222a76c9f7dbf30b80fb52d3e335a
Ripan Baidya <ripanbaidya7980@gmail.com>
2025-12-20 00:15:41 +0530

    sum of two numbers

---
+++ src/basics/SumOfTwoNumbers.java
@@ -0,0 +1,18 @@
+package basics;
+
+/*
+ * This program calculates the sum of two user-provided numbers.
+ */
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        // Take two number
+        int num1 = 10;
+        int num2 = 20;
+
+        // Calculating sum of two numbers
+        int sum = num1 + num2;
+
+        // Print the resultant sum
+        System.out.println("the sum is: "+sum);
+    }
+}

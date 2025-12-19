efe66fbdb87a19233079ab329c7e2bed6306f384
Ripan Baidya <ripanbaidya7980@gmail.com>
2025-12-20 00:13:56 +0530

    comments

---
+++ src/basics/Comments.java
@@ -0,0 +1,19 @@
+package basics;
+
+/*
+ * Comments:
+ *
+ * Comments are pieces of text in code that are ignored by the compiler and are used for documentation purposes.
+ * They are used to explain the code and provide information to other developers.
+ *
+ * There are two types of comments in Java:
+ * 1. Single line comment:
+ * 2. Multi line comment:
+ */
+public class Comments {
+    // This is a single line comment
+
+    /*
+     * This is multi line comment
+     */
+}
